05cec5755aecb9029d840804633aaa6f96aa05a2
161710108 <161710108@smkassalaambandung.sch.id>
2018-03-16 16:52:59 +0700

    rapihkan

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+
+@extends('layouts.app')
+@section('content')
+      <iframe align="center" frameborder="yes" height="609px" name="frame1" scrolling="auto" src="https://dashboard.tawk.to/#/chat" style="border: 1px solid;" width="1110px"></iframe> 
+@endsection
